951d509b13cc47b594506e848e780f7adc1c0110
GiSeong Lee <cj8928@gmail.com>
2025-06-17 18:17:12 +0900

    add: 테스트 홈 컨텐츠

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,18 @@
+import Button from "../components/atoms/button/Button";
+import Title from "../components/atoms/title/Title";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="min-h-screen flex flex-col gap-4 justify-center items-center">
+      <Title />
+      <div className="flex items-center gap-4">
+        <p className="font-bold text-white font-family-kr">
+          누군가에게 인사해보세요.
+        </p>
+        <Button onClick={() => console.log("hi")}>🖐</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
