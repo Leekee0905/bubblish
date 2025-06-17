ca1bcdd8ba101778cee60a6288177725bf92a185
GiSeong Lee <cj8928@gmail.com>
2025-06-17 18:16:59 +0900

    update: 배경 색상 변경

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 const Layout = () => {
   return (
-    <main className="w-full min-w-96 max-w-screen min-h-screen mx-auto px-5 md:px-8 lg:px-10 flex flex-col items-center relative">
+    <main className="w-full min-w-96 max-w-screen min-h-screen mx-auto px-5 md:px-8 lg:px-10 flex flex-col items-center relative bg-[#cedef2]">
       <Outlet />
     </main>
   );
