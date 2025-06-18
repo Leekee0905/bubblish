025ceab05cf5ff8b3f72732ead4f4c9e83796918
GiSeong Lee <cj8928@gmail.com>
2025-06-18 16:48:56 +0900

    update: atomic design pattern 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,7 @@
-import Button from "../components/atoms/button/Button";
-import Title from "../components/atoms/title/Title";
+import HomeGreetingSection from "../components/organism/home/HomeGreetingSection";
 
 const Home = () => {
-  return (
-    <div className="min-h-screen flex flex-col gap-4 justify-center items-center">
-      <Title />
-      <div className="flex items-center gap-4">
-        <p className="font-bold text-white font-family-kr">
-          누군가에게 인사해보세요.
-        </p>
-        <Button onClick={() => console.log("hi")}>🖐</Button>
-      </div>
-    </div>
-  );
+  return <HomeGreetingSection />;
 };
 
 export default Home;
