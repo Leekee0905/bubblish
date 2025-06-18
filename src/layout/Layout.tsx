f9b0ce250e272819dab3bf4d0f0b549a871a096d
GiSeong Lee <cj8928@gmail.com>
2025-06-18 16:48:29 +0900

    update: 폰트, 색상 이름 변경

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 const Layout = () => {
   return (
-    <main className="w-full min-w-96 max-w-screen min-h-screen mx-auto px-5 md:px-8 lg:px-10 flex flex-col items-center relative bg-[#cedef2]">
+    <main className="font-family-kr w-full min-w-auto max-w-screen min-h-screen mx-auto px-5 md:px-8 lg:px-10 flex flex-col items-center relative bg-lightblue text-[#3c4856]">
       <Outlet />
     </main>
   );
