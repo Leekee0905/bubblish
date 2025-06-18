b53aeacdac8c620e14c8ef8afa204b48b4f301a6
GiSeong Lee <cj8928@gmail.com>
2025-06-18 16:46:56 +0900

    update: 버튼 마우스 이벤트 추가

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -2,33 +2,42 @@ import type { ReactNode } from "react";
 import clsx from "clsx";
 interface ButtonProps {
   children: ReactNode;
+  className?: string;
   variant?: "primary" | "secondary";
   disabled?: boolean;
   onClick: () => void;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
 }
 
 const Button = ({
   children,
   variant = "primary",
   disabled = false,
+  className,
   onClick,
+  onMouseEnter,
+  onMouseLeave,
 }: ButtonProps) => {
   const base =
     "px-4 py-2 rounded-lg font-semibold transition-colors cursor-pointer";
   const styles = {
-    primary: "bg-[#cedef2] text-white hover:bg-[#b9c1dc]",
+    primary: "bg-lightblue hover:bg-[#91aecf]",
     secondary: "bg-gray-200 text-black hover:bg-gray-300",
-    ghost: "bg-transparent text-blue-500 hover:bg-blue-100",
   };
+
   return (
     <button
       className={clsx(
         base,
         styles[variant],
-        disabled && "opacity-50 cursor-not-allowed"
+        disabled && "opacity-50 cursor-not-allowed",
+        className
       )}
       disabled={disabled}
       onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
       {children}
     </button>
